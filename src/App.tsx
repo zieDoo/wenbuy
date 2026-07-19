d04dd81d972e1db3a8a6622149764809e9cf84b1
zieDoo <samuel.bednarikall@gmail.com>
2026-07-19 15:24:57 +0200

    Refactor state management to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
 import ETFCard from "./components/ETFCard";
 import { etfs } from "./data/etfs";
+import { useState } from "react";
+import type { ETF } from "./types/etf";
 
 const App = () => {
+  const [portfolio, setPortfolio] = useState(etfs);
+
+  const updateETF = (updatedETF: ETF) => {
+    const updatedPortfolio = portfolio.map((etf) => {
+      return etf.ticker === updatedETF.ticker ? updatedETF : etf;
+    });
+
+    setPortfolio(updatedPortfolio);
+  };
+
   return (
     <div>
       <h1>WenBuy</h1>
       <p>Know when to buy.</p>
 
-      <div className="dashboard">
+      {/* <div className="dashboard">
         {etfs.map((etf) => (
           <ETFCard key={etf.ticker} etf={etf} />
+        ))}
+      </div> */}
+
+      <div className="dashboard">
+        {portfolio.map((etf) => (
+          <ETFCard key={etf.ticker} etf={etf} updateETF={updateETF} />
         ))}
       </div>
     </div>
--- src/components/ETFCard.tsx
+++ src/components/ETFCard.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import type { ETF } from "../types/etf";
 import "./ETFCard.css";
 
 type ETFCardProps = {
   etf: ETF;
+  updateETF(etf: ETF): void;
 };
 
-const ETFCard = ({ etf }: ETFCardProps) => {
+const ETFCard = ({ etf, updateETF }: ETFCardProps) => {
+  // aktualna hodnota = useState(default hodnota)
+  // const [changeValue, setValueChange] = useState(etf.change);
+
   return (
     <div className="etf-card">
       <h2>{etf.name}</h2>
@@ -15,6 +20,10 @@ const ETFCard = ({ etf }: ETFCardProps) => {
         {etf.change > 0 ? "+" : ""}
         {etf.change}%
       </p>
+      {/* <button onClick={() => setValueChange((prevValue) => prevValue + 0.1)}> */}
+      <button onClick={() => updateETF({ ...etf, change: etf.change + 0.1 })}>
+        change
+      </button>
     </div>
   );
 };
