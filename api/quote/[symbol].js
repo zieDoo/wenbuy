c7ab81a853f361b515257325ee25afbba73de975
zieDoo <samuel.bednarikall@gmail.com>
2026-08-10 00:10:01 +0200

    Updating UI of ETF search bar

--- api/quote/[symbol].js
+++ api/quote/[symbol].js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
     const quote = await yahooFinance.quote(symbol);
 
-    console.log("YAHOO QUOTE: ", quote);
+    // console.log("YAHOO QUOTE: ", quote);
 
     res.status(200).json({
       symbol: quote.symbol,
